1f654fc596d319f4d9c8decfe91a9cecca96e527
byeonyonghyun~ <qusdydgus0113@gmail.com>
2024-03-13 18:31:49 +0900

    240313

--- 매장안내/script.js
+++ 매장안내/script.js
@@ -1,15 +1,19 @@
 import stores from "./stores.js";
 
 const form = document.querySelector("form");
-const services = document.querySelectorAll("input[type='checkbox]");
+const services = document.querySelectorAll("input[type='checkbox']");
 const locates = document.querySelectorAll(".locate_name");
 
 const city = document.querySelector("#city")
 const state = document.querySelector("#state");
+const name = document.querySelector("#store_name");
 
 const pages = document.querySelectorAll(".page_number a");
 
 
+
+
+//스크롤시 fade-in클래스가 포함된 section에 active클래스를 추가
 const activeEvent = (e) => {
   const active = document.querySelector(".active");
   e.preventDefault();
@@ -17,6 +21,8 @@ const activeEvent = (e) => {
   e.target.classList.add("active");
 };
 
+
+//table 리스트 전체 제거
 const removeItems = () => {
   const items = document.querySelectorAll("tbody tr");
   items.forEach((item) => {
@@ -24,34 +30,58 @@ const removeItems = () => {
   });
 };
 
+//table리스트 생성
 const createItem = function (store) {
   const tbody = document.querySelector("tbody");
-
   const tr = document.createElement("tr");
-  const img = document.createElement("img");
 
-  const attr = document.createAttribute("src");
-  attr.value = store.src;
-  img.setAttributeNode(attr);
+  for (let i = 0; i < 5; i++) {
+    const td = document.createElement("td");
 
-  tr.innerHTML = `
-  <td class=store_nm>${store.name}</td>
-  <td class=store_loc>${store.locate}</td>
-  <td class=store_adr>${store.adrress}</td>
-  <td class=store_tel>${store.tel}</td>
-  <td class=store_srv>${store.srv}</td>
-  `
+    switch (i) {
+      case 0:
+        td.innerText = `${store.name}`
+        break;
+      case 1:
+        td.innerText = `${store.locate}`
+        break;
+      case 2:
+        td.innerText = `${store.adrress}`
+        break;
+      case 3:
+        td.innerText = `${store.tel}`
+        break;
+      case 4:
+        for (let i = 0; i < store.srv.length; i++) {
+          const img = document.createElement("img");
+          const attr = document.createAttribute("src");
+          attr.value = store.srv[i];
+          img.setAttributeNode(attr);
+          td.append(img);
+        }
+        break;
+    }
+
+    tr.append(td);
+  }
+  // tr.innerHTML = `
+  // <td>${store.name}</td>
+  // <td>${store.locate}</td>
+  // <td>${store.adrress}</td>
+  // <td>${store.tel}</td>
+  // <td>${store.srv}</td>
+  // `
+
   tbody.appendChild(tr);
 };
 
-
+//select태그에서 option요소를 선택했을 때, 값에 따라 연결되어 있는 select태그의 option값 
 const selectCity = () => {
   const selectedCity = city.value;
 
-
   if (selectedCity === "강원") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="강릉시">강릉시</option>
     <option value="고성군">고성군</option>
     <option value="동해시">동해시</option>
@@ -69,10 +99,10 @@ const selectCity = () => {
     <option value="홍천군">홍천군</option>
     <option value="화천군">화천군</option>
     <option value="횡성군">횡성군</option>
-    `
+  `
   } else if (selectedCity === "경기") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="가평군">가평군</option>
     <option value="고양시">고양시</option>
     <option value="과천시">과천시</option>
@@ -101,10 +131,10 @@ const selectCity = () => {
     <option value="평택시">평택시</option>
     <option value="포천시">포천시</option>
     <option value="하남시">하남시</option>
-    `
+  `
   } else if (selectedCity === "경남") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="거제시">거제시</option>
     <option value="고성군">고성군</option>
     <option value="남해군">남해군</option>
@@ -113,43 +143,43 @@ const selectCity = () => {
     <option value="진주시">진주시</option>
     <option value="하동군">하동군</option>
     <option value="함안군">함안군</option>
-    `
+  `
   } else if (selectedCity === "경북") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="경주시">경주시</option>
     <option value="안동시">안동시</option>
     <option value=영주시">영주시</option>
-    <option value="예천군">예천군</option>
-    `
+    < option value = "예천군" > 예천군</ >
+      `
   } else if (selectedCity === "광주") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
-    `
+    <option value = "구/군 선택" > 구/군 선택</option>
+        `
   } else if (selectedCity === "대구") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
-    <option value="달성군">달성군</option>
-    `
+        < option value = "구/군 선택" > 구 / 군 선택</ >
+          <option value="달성군">달성군</option>
+  `
   } else if (selectedCity === "대전") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="유성구">유성구</option>
     <option value="서구">서구</option>
-    `
+  `
   } else if (selectedCity === "부산") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="기장군">기장군</option>
     <option value="남구">남구</option>
     <option value="동래구">동래구</option>
     <option value="수영구">수영구</option>
     <option value="부산진구">부산진구</option>
     <option value="사하구">사하구</option>
-    `
+  `
   } else if (selectedCity === "서울") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="강남구">강남구</option>
     <option value="강북구">강북구</option>
     <option value="강서구">강서구</option>
@@ -169,20 +199,20 @@ const selectCity = () => {
     <option value="송파구">송파구</option>
     <option value="종로구">종로구</option>
     <option value="중구">중구</option>
-    `
+  `
   } else if (selectedCity === "세종") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
-    `
+    <option value = "구/군 선택" > 구/군 선택</option>
+      `
   } else if (selectedCity === "울산") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="남구">남구</option>
     <option value="중구">중구</option>
-    `
+  `
   } else if (selectedCity === "인천") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="강화군">강화군</option>
     <option value="계양구">계양구</option>
     <option value="남구">남구</option>
@@ -192,32 +222,32 @@ const selectCity = () => {
     <option value="연수구">연수구</option>
     <option value="옹진구">옹진구</option>
     <option value="중구">중구</option>
-    `
+  `
   } else if (selectedCity === "전남") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="광양시">광양시</option>
     <option value="강진군">강진군</option>
     <option value="목포시">목포시</option>
     <option value="순천시">순천시</option>
     <option value="여수시">여수시</option>
-    `
+  `
   } else if (selectedCity === "전북") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="군산시">군산시</option>
     <option value="익산시">익산시</option>
     <option value="전주시">전주시</option>
-    `
+  `
   } else if (selectedCity === "제주") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="제주시">제주시</option>
     <option value="서귀포시">서귀포시</option>
-    `
+  `
   } else if (selectedCity === "충남") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="계룡시">계룡시</option>
     <option value="당진시">당진시</option>
     <option value="보령시">보령시</option>
@@ -227,17 +257,17 @@ const selectCity = () => {
     <option value="태안군">태안군</option>
     <option value="홍성군"></option>
     <option value="홍성군"></option>
-    `
+  `
   } else if (selectedCity === "충북") {
     state.innerHTML = `
-    <option value="구/군 선택">구/군 선택</option>
+    <option value = "구/군 선택" > 구/군 선택</option>
     <option value="제천시">제천시</option>
     <option value="청주시">청주시</option>
-    `
+  `
   }
 }
 
-
+//지도에서 span태그를 클릭했을 때 select태그와 연결 
 locates.forEach((locate) => {
   locate.addEventListener("click", (e) => {
     const active = document.querySelector(".active");
@@ -257,9 +287,23 @@ locates.forEach((locate) => {
     });
 
     removeItems();
-    filtered.forEach((store) => {
-      createItem(store);
-    })
+
+    if (filtered.length === 0) {
+      const tbody = document.querySelector("tbody");
+      const tr = document.createElement("tr");
+      for (let i = 0; i < 5; i++) {
+        const td = document.createElement("td");
+        if (i === 2) {
+          td.innerText = "등록된 매장이 없습니다!"
+        }
+        tr.append(td);
+      }
+      tbody.append(tr);
+    } else {
+      filtered.forEach((store) => {
+        createItem(store);
+      });
+    }
 
     selectCity();
   });
@@ -267,7 +311,36 @@ locates.forEach((locate) => {
 
 const searchResult = (e) => {
   e.preventDefault();
+  city.value
+  state.value
+  name.value
+  services.forEach((service) => {
+    service.addEventListener("change", function (e) {
+      return e.target.id;
+    })
+  })
+  const filtered = stores.data.filter((store) => {
+    return store.locate === locateId;
+  });
 
+  removeItems();
+
+  if (filtered.length === 0) {
+    const tbody = document.querySelector("tbody");
+    const tr = document.createElement("tr");
+    for (let i = 0; i < 5; i++) {
+      const td = document.createElement("td");
+      if (i === 2) {
+        td.innerText = "등록된 매장이 없습니다!"
+      }
+      tr.append(td);
+    }
+    tbody.append(tr);
+  } else {
+    filtered.forEach((store) => {
+      createItem(store);
+    });
+  }
 }
 
 const importData = () => {
--- 매장안내/stores.js
+++ 매장안내/stores.js
@@ -22,11 +22,11 @@ const stores = {
       adrress: "인천 서구 청라한내로 110 (청라동) 1층",
       tel: "070-8657-3336",
       srv: [
-        wifi,
-        park,
-        smoke,
-        parasol,
-        mobile,
+        './img/wifi.svg',
+        './img/park.svg',
+        './img/smoke.svg',
+        './img/parasol.svg',
+        './img/mobile.svg'
       ]
     },
     {
@@ -35,9 +35,7 @@ const stores = {
       adrress: "강원특별자치도 영월군 영월읍 청령포로 111 (방절리) 1층, 셀렉토커피 영월점",
       tel: "033-373-5557",
       srv: [
-        wifi,
-        park,
-        parasol,
+        './img/wifi.svg'
       ]
     },
     {
@@ -46,6 +44,7 @@ const stores = {
       adrress: "서울 도봉구 마들로11가길 16 (창동, KING 프라자) 1층",
       tel: "029940313",
       srv: [
+        './img/wifi.svg'
       ]
     },
     {
@@ -56,6 +55,42 @@ const stores = {
       srv: [
       ]
     },
+    {
+      name: "부천역점",
+      locate: "경기",
+      adrress: "경기 부천시 부일로449번길 6 (심곡동) 1층 셀렉토커피 부천역점",
+      tel: "032-652-7485",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "제주도남점",
+      locate: "제주",
+      adrress: "제주특별자치도 제주시 도남로 53 (도남동) 1층 셀렉토커피 제주도남점",
+      tel: "064-725-2477",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "포세카신규캠프점",
+      locate: "제주",
+      adrress: "경기 평택시 첨단대로 280 (고덕동) 포세카신규캠프점",
+      tel: "064-725-2477",
+      srv: [
+      ]
+    },
+    {
+      name: "거제옥포점",
+      locate: "경남",
+      adrress: "경남 거제시 옥포로 203 (옥포동) 1, 2층",
+      tel: "055-950-7867",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+
   ]
 }
 console.log(stores)
