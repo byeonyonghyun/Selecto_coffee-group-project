f16c36a1c01c4e1f100c9141359e7e8823167094
byeonyonghyun~ <qusdydgus0113@gmail.com>
2024-03-21 18:35:22 +0900

    대규모 변경 v2

---
+++ js/stores.js
@@ -0,0 +1,98 @@
+//wifi = './img/wifi.svg'
+//park = './img/park.svg'
+//smoke = './img/smoke.svg'
+//parasol = './img/parasol.svg'
+//mobile = './img/mobile.svg'
+
+const stores = {
+  data: [
+    {
+      name: "평택서재자이점",
+      locate: "경기",
+      address: "경기 평택시 동삭동 715-1 1층",
+      tel: "031-625-1777",
+      srv: [
+        './img/wifi.svg',
+        './img/parasol.svg',
+      ]
+    },
+    {
+      name: "청라국제도시점",
+      locate: "인천",
+      address: "인천 서구 청라한내로 110 (청라동) 1층",
+      tel: "070-8657-3336",
+      srv: [
+        './img/wifi.svg',
+        './img/park.svg',
+        './img/smoke.svg',
+        './img/parasol.svg',
+        './img/mobile.svg'
+      ]
+    },
+    {
+      name: "영월점",
+      locate: "강원",
+      address: "강원특별자치도 영월군 영월읍 청령포로 111 (방절리) 1층, 셀렉토커피 영월점",
+      tel: "033-373-5557",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "창동역점",
+      locate: "서울",
+      address: "서울 도봉구 마들로11가길 16 (창동, KING 프라자) 1층",
+      tel: "029940313",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "삼성전자 화성캠퍼스점",
+      locate: "경기",
+      address: "경기 화성시 삼성전자로 1 (반월동) 셀렉토커피 삼성전자 화성캠퍼스점",
+      tel: "",
+      srv: [
+      ]
+    },
+    {
+      name: "부천역점",
+      locate: "경기",
+      address: "경기 부천시 부일로449번길 6 (심곡동) 1층 셀렉토커피 부천역점",
+      tel: "032-652-7485",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "제주도남점",
+      locate: "제주",
+      address: "제주특별자치도 제주시 도남로 53 (도남동) 1층 셀렉토커피 제주도남점",
+      tel: "064-725-2477",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+    {
+      name: "포세카신규캠프점",
+      locate: "경기",
+      address: "경기 평택시 첨단대로 280 (고덕동) 포세카신규캠프점",
+      tel: "064-725-2477",
+      srv: [
+      ]
+    },
+    {
+      name: "거제옥포점",
+      locate: "경남",
+      address: "경남 거제시 옥포로 203 (옥포동) 1, 2층",
+      tel: "055-950-7867",
+      srv: [
+        './img/wifi.svg'
+      ]
+    },
+
+  ]
+}
+console.log(stores)
+
+export default stores
